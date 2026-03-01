a5b07b0e0c087ef9557f452eb68640e51b84d216
G4l3S <gabs.sumergido@gmail.com>
2026-03-01 21:35:22 +0800

    feat: updated damage text to also indicate healing

--- scripts/ui/damage_text.gd
+++ scripts/ui/damage_text.gd
@@ -7,16 +7,18 @@ signal finished
 func _ready() -> void:
 	var f = create_tween().set_parallel()
 	f.tween_property(self, "modulate:a", 0.0, duration)
-	f.tween_property(self, "scale", Vector2(0.6, 0.6), duration)
-	f.tween_property(self, "position", Vector2(position.x, 50), duration)
+	f.tween_property(self, "position", Vector2(position.x, -25), duration)
 	await f.finished
 	finished.emit()
 	queue_free()
 
-func amount(x : float, v : float) -> void:
-	$Label.text = str(int(x))
-	match v:
-		2.0:
-			modulate = Color(1.0, 0.678, 0.322, 1.0)
-		0.5:
-			modulate = Color(0.619, 0.711, 1.0, 1.0)
+func amount(x : float, v : float, h : bool) -> void:
+	$Label.text = str(int(abs(x)))
+	if h:
+		match v:
+			2.0:
+				modulate = Color(1.0, 0.678, 0.322, 1.0)
+			0.5:
+				modulate = Color(0.619, 0.711, 1.0, 1.0)
+	else:
+		modulate = Color(0.0, 0.898, 0.0, 1.0)
