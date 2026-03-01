6dee9ecc67d6042618551a593c56050602089a37
G4l3S <gabs.sumergido@gmail.com>
2026-03-01 21:36:11 +0800

    misc: minor code changes for better readability

--- scripts/resource/effect.gd
+++ scripts/resource/effect.gd
@@ -5,22 +5,29 @@ class_name Effect
 
 signal effect_finished
 
+@export var formula : ValueFormula
+
+@export_group("Targeting Settings")
 @export var inherit_target : bool :
 	set(value):
 		inherit_target = value
-		self.notify_property_list_changed()
+		notify_property_list_changed()
 
+@export var target_group : Ability.TargetGroup :
+	set(value):
+		target_group = value
+		notify_property_list_changed()
 @export var target_mode : Ability.TargetMode
-@export var target_group : Ability.TargetGroup
-const additional : Array[StringName] = [&"target_mode", &"target_group"]
 
 func _validate_property(property: Dictionary) -> void:
-	if property.name in additional:
-		if inherit_target == true:
-			property.usage = PROPERTY_USAGE_NO_EDITOR
-		else:
-			property.usage = PROPERTY_USAGE_DEFAULT
-
-@export var formula : ValueFormula
+	var hide = false
+	match property.name:
+		"target_group", "target_mode":
+			hide = inherit_target
+	
+	if hide:
+		property.usage &= ~PROPERTY_USAGE_DEFAULT
+	else:
+		property.usage |= PROPERTY_USAGE_DEFAULT
 
 @abstract func trigger(source : Entity, recipient : Entity) -> void
